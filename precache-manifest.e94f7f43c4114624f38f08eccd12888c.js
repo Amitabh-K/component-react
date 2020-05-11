fa9462755250d0a14e906e9df40d970f8fbf6b66
Amitabh Kumar <amitabh.kumar2@gmail.com>
2020-05-10 20:16:36 -0400

    Updates

---
+++ precache-manifest.e94f7f43c4114624f38f08eccd12888c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f077e870e8caa487f34",
+    "url": "/component-react/static/js/main.4f077e87.chunk.js"
+  },
+  {
+    "revision": "347d60cfb1102d6f5dcf",
+    "url": "/component-react/static/js/1.347d60cf.chunk.js"
+  },
+  {
+    "revision": "f3cf9e897c6003b0b49e",
+    "url": "/component-react/static/js/runtime~main.f3cf9e89.js"
+  },
+  {
+    "revision": "935b497ae68b7197a5034f4feda83f27",
+    "url": "/component-react/index.html"
+  }
+];
